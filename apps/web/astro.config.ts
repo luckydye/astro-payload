375eb604786dd15bcc0e87836a30a12a25f3c81b
Tim Havlicek <t.havlicek@s-v.de>
2023-04-08 14:53:23 +0200

    cleanup and fixes

--- apps/web/astro.config.ts
+++ apps/web/astro.config.ts
@@ -8,12 +8,13 @@ export default defineConfig({
 		serverEntry: "server.mjs",
 	},
 	adapter: payload({
-		secret: process.env.PAYLOAD_SECRET,
-		mongoURL: process.env.DB_URI,
+		secret: process.env.PAYLOAD_SECRET || "",
+		mongoURL: process.env.DB_URI || "",
 		mongoOptions: {
 			user: process.env.DB_ROOT_USER,
 			pass: process.env.DB_ROOT_PASS,
 			dbName: process.env.DB_NAME,
 		},
+		configPath: import.meta.env.DEV ? "./payload.config.ts" : "dist/payload.config.js",
 	}),
 });
--- packages/@payload/astro/types.ts
+++ packages/@payload/astro/types.ts
@@ -1,7 +1,11 @@
 import { SSRManifest } from "astro";
 import type { InitOptions } from "payload/config";
 
-export type PayloadOptions = InitOptions | Promise<InitOptions>;
+export type AdapterInitOptions = {
+	configPath: string;
+};
+
+export type PayloadOptions = (InitOptions & AdapterInitOptions) | Promise<InitOptions & AdapterInitOptions>;
 
 export interface ExtendedSSRManifest extends SSRManifest {
 	payloadInitOptions: PayloadOptions;
